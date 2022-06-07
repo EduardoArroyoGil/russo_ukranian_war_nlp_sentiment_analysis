73acdf4418cbbbe611fe07c915b22759484e2ec3
EduardoArroyoGil <eduardoarroyogil@gmail.com>
2022-06-07 17:38:37 +0200

    correcting naming

--- etls_main.py
+++ etls_main.py
@@ -11,14 +11,14 @@ from tqdm.auto import tqdm
 twitter_conn = twitter_connection.Twitter(bearer_token='AAAAAAAAAAAAAAAAAAAAAHAhdQEAAAAAqIbsC3QZW5jWqDu8%2FF2g%2BhTStug%3Dq4S5oh53c0j6dlYJRpsVwsKdHe6xuqDIECKtwHonL9g8zAshi0')
 # gpt3_conn = openai_connection.GPT3(api_key='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')
 gpt3_transformation = openai_trasnformation.GPT3Transformation(api_key='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')
-twitter_transformation = twitter_transformation.TwitterUtils()
+twitter_utils = twitter_transformation.TwitterUtils()
 
 #  EMOTIONAL ANALYSIS FOR EACH TWEET
 twitter_response, twitter_df = twitter_conn.search_recent_tweet(text_to_seearch='Guerra de Ucrania')
 
 
 tqdm.pandas(desc="ETL is chekcing viability of accounts.")
-twitter_df["is_reclaimable"] = twitter_df.progress_apply(lambda x: twitter_transformation.check_correct_acc_tw_association(x.account_id, x.account_id_check), axis=1)
+twitter_df["is_reclaimable"] = twitter_df.progress_apply(lambda x: twitter_utils.check_correct_acc_tw_association(x.account_id, x.account_id_check), axis=1)
 
 tqdm.pandas(desc="GPT3 is translating.")
 twitter_df["tweet_text_translated_gpt3"] = twitter_df["tweet_text"].progress_apply(gpt3_transformation.translate_to_english)
